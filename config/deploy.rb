a2bc62e32c96ed2f4b8534ec84aa8c3d0df0ddf6
HaiTran <tranhaicoder@gmail.com>
2024-01-27 17:31:25 +0700

    Update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 lock "~> 3.17.0"
 
 set :user, 'deploy'
-set :application, "chat-api"
+set :application, "chat-room-api"
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :keep_releases, 2
 
-set :linked_files, %w(config/database.yml config/master.key)
+set :linked_files, %w(config/database.yml config/master.key .env)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 
 set :pty,             true
@@ -20,7 +20,7 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_conf,    -> {"#{shared_path}/puma.rb"}
 set :puma_access_log, "#{release_path}/log/puma.access.log"
 set :puma_error_log,  "#{release_path}/log/puma.error.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/dekisugi_id_rsa.pub) }
 # keys on local, paste it to authorized_keys on server
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server '13.231.143.62', port: 22, roles: [:web, :app], primary: true
+server '35.77.230.55', port: 22, roles: [:web, :app], primary: true
 set :repo_url, "git@github.com:haitran1996/chat-room-api.git"
 set :stage, :staging
 set :branch, :master

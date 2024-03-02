e1cf629c68c5b4bc329aef1c830d023a19019365
HaiTran <tranhaicoder@gmail.com>
2024-03-02 15:39:12 +0700

    Change push notification to Firebase Cloud Messaging API (V1)

--- app/services/notifications/push_message_notification.rb
+++ app/services/notifications/push_message_notification.rb
@@ -6,18 +6,58 @@ class Notifications::PushMessageNotification < ApplicationService
   end
 
   def call
-    fcm = FCM.new(ENV['FIREBASE_SEVER_KEY'])
-    fcm.send(@message.recipients.pluck(:device_token), notification_options)
+    fcm = FCM.new(
+      ENV['API_KEY'],
+      ENV['FIREBASE_CREDENTIALS_PATH'],
+      ENV['FIREBASE_PROJECT_ID'],
+    )
+    @message.recipients.pluck(:device_token).each do |token|
+      fcm.send_v1(message(token))
+    end
   end
 
   private
 
-  def notification_options
-    {
-      notification: {
-        title: "You have a new message from #{@message.user.name}!",
-        body: @message.content
+  def message(device_token, options = {})
+    message = {
+      # 'topic': "89023", # OR token if you want to send to a specific device
+      'token': device_token,
+      # 'data': {
+      #   payload: payload_json
+      # },
+      'notification': notification,
+      'android': {
+        'notification': {
+          "click_action": "TOP_STORY_ACTIVITY",
+          **notification
+        }
+      },
+      'apns': {
+        payload: {
+          aps: {
+            sound: "default",
+            category: "#{Time.zone.now.to_i}"
+          }
+        }
+      },
+      'fcm_options': {
+        analytics_label: 'Label'
       }
     }
+  end
+
+  def notification
+    {
+      title: "You have a new message from #{@message.user.name}!",
+      body: @message.content
+    }
+  end
+
+  def payload_json
+    {
+      data: {
+        id: 1
+      }
+    }.to_json
   end
 end
